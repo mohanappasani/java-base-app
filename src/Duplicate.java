052e1405d3595bc019f55601eb24561c44250cd7
Mohan Appasani <mohan@bizlog.com>
2023-10-05 21:40:55 +0530

    test v3 updated

--- src/Duplicate.java
+++ src/Duplicate.java
@@ -7,13 +7,18 @@ public class Duplicate {
 		
 		for (int i =0;i<arr.length;i++) {
 	  for(int j = i + 1; j < arr.length; j++)	{
+
+		System.out.println("For loop iteration started..");
+
+
 		  if (arr [i] == arr [j])
 		  {
 			  System.out.println("xyz" + arr[j]);
 			  System.out.println("Hii..");
 			  System.out.println("Hii..");
-			  System.out.println("Hii..");
-	
+			  System.out.println("Hii..");	
+
+			  System.out.println("Hii..World");	
 		  }
 	  }
 			
