e52d0e8e2c4971372f352b050d8a9dcb96f468d2
Mohan Krishna Appasani <81454700+mohanappasani@users.noreply.github.com>
2023-10-05 21:28:40 +0530

    Update Duplicate.java

--- src/Duplicate.java
+++ src/Duplicate.java
@@ -9,7 +9,7 @@ public class Duplicate {
 	  for(int j = i + 1; j < arr.length; j++)	{
 		  if (arr [i] == arr [j])
 		  {
-			  System.out.println(arr[j]);
+			  System.out.println("xyz"+ arr[j]);
 		  }
 	  }
 			
